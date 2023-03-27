050e7d923a373d3f320d2b60403c03cbe4da855d
JonathanRB <jvanegasc001@alumno.uaemex.mx>
2023-03-27 01:01:26 -0600

    coneccion

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const mongodb_URI = 
-process.env.mongodb_URI || "mongodb://localhost/test";
+process.env.mongodb_URI || "mongodb://mongo:cxkfZOHKKHXzngfASgWP@containers-us-west-146.railway.app:7824";
+//"mongodb://localhost/test";
 
 export const PORT = process.env.PORT || 5000;
